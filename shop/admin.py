8263e308e4efc6352db5c7cd08a2a7dfddceb0c7
Deflaus <snow41521@gmail.com>
2021-01-12 13:46:37 +0300

    adding admin views

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'slug',
+    ] # Поля, отображаемые на странице списка объектов
+    prepopulated_fields = {
+        'slug': ('name',)
+    } # Определение slug, основываясь на названии категории
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'slug',
+        'price',
+        'available',
+        'created',
+        'updated',
+    ] # Поля, отображаемые на странице списка объектов
+    list_filter = [
+        'available',
+        'created',
+        'updated',
+    ] # Фильтр данных
+    list_editable = [
+        'price',
+        'available',
+    ] # Поля, которые можно будет редактировать на странице списка объектов
+    prepopulated_fields = {
+        'slug': ('name',)
+    } # Определение slug, основываясь на названии товара
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    '''
+    Модель категории товаров
+    '''
+    name = models.CharField(max_length=200, db_index=True) # Название категории
+    slug = models.SlugField(max_length=200, unique=True) # Slug категории
+
+    class Meta:
+        ordering = ('name',) # Сортировка по названию категории
+        verbose_name = 'category' # Название модели в ед. числе
+        verbose_name_plural = 'categories' # Название модели во мн. числе
+    
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    '''
+    Модель Товара
+    '''
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE) # Внешний ключ категория товара
+    name = models.CharField(max_length=200, db_index=True) # Название товара
+    slug = models.SlugField(max_length=200, db_index=True) # Slug товара
+    image = models.ImageField(upload_to='products', blank=True) # Изображение товара
+    description = models.TextField(blank=True) # Описание товара
+    price = models.DecimalField(max_digits=10, decimal_places=2) # Цена товара
+    available = models.BooleanField(default=True) # Наличие товара
+    created = models.DateTimeField(auto_now_add=True) # Создание товара
+    updated = models.DateTimeField(auto_now=True) # Изменение товара 
+
+    class Meta:
+        ordering = ('name',) # Сортировка по названию товара
+        index_together = (('id', 'slug'),) # Объединение двух индексов в один
+    
+    def __str__(self):
+        return self.name
