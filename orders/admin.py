81da4b648eab80b267314e200d1742bf8151cf6c
Deflaus <snow41521@gmail.com>
2021-01-14 18:01:39 +0300

    adding admin for Order and OrderItem

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    '''
+    Класс для модели OrderItem, чтобы добавить ее в  виде списка
+    связанных объектов на страницу заказа, зарегистрированную
+    через OrderAdmin
+    '''
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'address',
+        'postal_code',
+        'city',
+        'paid',
+        'created',
+        'updated',
+    ]
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
