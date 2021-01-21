0064804c530d092cca290016dc50ad1ef4227b6e
Deflaus <snow41521@gmail.com>
2021-01-21 20:56:59 +0300

    creating model of Coupon and adding her in admin panel

--- coupons/models.py
+++ coupons/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True) # код купона
+    valid_from = models.DateTimeField() # дата и время начала действия купона
+    valid_to = models.DateTimeField() # дата и время окончания действия купона
+    discount = models.IntegerField( 
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    ) # размер скидки в процентах, принимающий значения от 0 до 100
+    active = models.BooleanField() # активность купона
+
+    def __str__(self):
+        return self.code
